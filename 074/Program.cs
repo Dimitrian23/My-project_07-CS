9729fb2382afb1fb0a0c17e1857e1c5a4793fc11
Dmitrian23 <dimonio1991@mail.ru>
2023-02-21 18:01:39 +0300

    Задача 074

---
+++ 074/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача_№74 Найти сумму цифр числа
+
+ int SumNumbers(int N)
+   {
+        if (N == 0)
+          return 0; 
+           return (N % 10 + SumNumbers (N / 10)); 
+    }
+ 
+int N=123;
+int sum = SumNumbers(N);
+Console.WriteLine($"Сумма цифр в числе {N} = {sum}");
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
