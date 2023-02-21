33620238e16a549faa5769075676b5c467d2c205
Dmitrian23 <dimonio1991@mail.ru>
2023-02-21 18:01:10 +0300

    Задача 071

---
+++ 071/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача_№71 Показать натуральные числа от N до 1, N задано
+
+void Pool(int i, int N)
+{
+    System.Console.Write($"{N,4}");
+       if(N>i)Pool(i,N-1);
+}
+
+// System.Console.WriteLine("Введите значения для переменной N:");
+// int N=Convert.ToInt32(Console.ReadLine());
+
+Pool(1,20);
+
+
+
+
+
