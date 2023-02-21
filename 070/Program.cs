1044c3b1b2b6b7c87c265ae72d22033485df3d07
Dmitrian23 <dimonio1991@mail.ru>
2023-02-21 18:01:01 +0300

    Задача 070

---
+++ 070/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача_№70 Показать натуральные числа от 1 до N, N задано
+
+void Loop (int i,int N)
+{
+    System.Console.Write($"{i,4},");
+    if(i<N)Loop(i+1,N);
+}
+
+System.Console.WriteLine("Введите значение для переменной N:");
+int N=Convert.ToInt32(Console.ReadLine());
+
+Loop(1,N);
+
+
+
+
+
+
+
+
+
